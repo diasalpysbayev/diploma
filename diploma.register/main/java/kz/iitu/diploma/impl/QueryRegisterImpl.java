ed2d4833a99a0c41ea11c8b8310f6c101bb5b6ee
diasalpysbayev <alpysbayevdias01@gmail.com>
2022-03-08 20:40:22 +0600

    feat: added query register impl

---
+++ diploma.register/main/java/kz/iitu/diploma/impl/QueryRegisterImpl.java
@@ -0,0 +1,16 @@
+package kz.iitu.diploma.impl;
+
+import kz.iitu.diploma.model.query.QueryRecord;
+import kz.iitu.diploma.register.QueryRegister;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class QueryRegisterImpl implements QueryRegister {
+
+  @Override
+  public void createQuery(QueryRecord queryRecord) {
+
+  }
+}
